37c9321e1a665c8936b6318479efaf273ccbfa34
Emily <Emily@10.143.129.91>
2015-04-08 17:00:40 -0700

    added methods to diagnosis

--- alertLife/src/alertLife/Diagnosis.java
+++ alertLife/src/alertLife/Diagnosis.java
@@ -20,6 +20,7 @@ public class Diagnosis {
 	private int symptom4;
 	private int symptom5;
 	private String comment;
+	private String doctor;
 	
 	public int getSymptom(int num) //prototyped for the Patient object, modify comment when fully implemented
 	{
@@ -38,5 +39,30 @@ public class Diagnosis {
 		return comment;
 	}
 	
-
+	public String getDoctor()
+	{
+		return doctor;
+	}
+	
+	public void setSymptom(int s, int value) //prototyped for the Patient object, modify comment when fully implemented
+	{
+		switch (s)
+		{
+			case 1 : symptom1 = value;
+			case 2 : symptom2 = value;
+			case 3 : symptom3 = value;
+			case 4 : symptom4 = value;
+			case 5 : symptom5 = value;
+		}
+	}
+	
+	public void setComment(String c)
+	{
+		comment = c;
+	}
+	
+	public void setDoctor(String d)
+	{
+		doctor = d;
+	}
 }
